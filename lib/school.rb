1bebaac06529be412a970215d285f552cf3fec3b
Michelle Moon <michellemoon236@gmail.com>
2019-04-18 19:07:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,10 +13,10 @@ class School
   end
   
   def add_student (name, grade)
-    # @name = name
-    # @grade = grade
-    ROSTER[grade] ||= []
-    ROSTER[grade] << "#{name}"
+    @name = name
+    @grade = grade
+    ROSTER[@grade] ||= []
+    ROSTER[@grade] << "#{@name}"
   end
   
   def grade (grade)
